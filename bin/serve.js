13a365bf8dccea9b1528047b5b45dcce134561b5
Nicolas Duvauchel <njlgad@corefiling.com>
2017-04-06 17:09:04 +0100

    Remove restriction to localhost

--- bin/serve.js
+++ bin/serve.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 app.use('/', express.static(path.join(__dirname, '../www')));
 
-const server = app.listen(8080, 'localhost', () => {
-  const address = server.address();
-  console.log(`App listening at http://${address.address}:${address.port}`);
+const server = app.listen(8080, () => {
+  console.log(`App listening on port ${server.address().port}`);
 })
