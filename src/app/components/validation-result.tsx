7c0f077574ffc1693d02f135775068b9238a3fb2
Damian Cugley <pdc@corefiling.com>
2017-09-07 15:24:24 +0100

    Add styling of validation result.

--- src/app/components/validation-result.tsx
+++ src/app/components/validation-result.tsx
@@ -4,14 +4,24 @@ import { Component, Props } from 'react';
 
 import './validation-result.less';
 
+type ValidationStatus = 'pass' | 'fail';
+
+const textByStatus = {
+  pass: 'Your document has been passed as valid XBRL.',
+  fail: 'Your document is not valid XBRL. Contact us to find out more!',
+};
+
 export interface ValidationResultProps extends Props<ValidationResult> {
-  status: 'pass' | 'fail';
+  status: ValidationStatus;
 }
 
 export default class ValidationResult extends Component<ValidationResultProps> {
   render(): JSX.Element {
     const { status } = this.props;
 
-    return <div className={classNames('cflbv-ValidationResult', `cflbv-ValidationResult-${status}`)}>{status}</div>;
+    return <div className={classNames('cflbv-ValidationResult', `cflbv-ValidationResult-${status}`)}>
+      <b className={classNames('cflbv-ValidationResult-status', `cflbv-ValidationResult-${status}Status`)}>{status}</b>
+      <p className={classNames('cflbv-ValidationResult-detail', `cflbv-ValidationResult-${status}Detail`)}>{textByStatus[status]}</p>
+    </div>;
   }
 }
