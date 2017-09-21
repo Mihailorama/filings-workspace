ca4de8098475b6996fd67f9133f55c8e21897a59
Matthew Law <mjl@corefiling.com>
2017-09-21 15:58:17 +0100

    Update publish script to use same CI version for the default docker image tag

--- tools/publish.sh
+++ tools/publish.sh
@@ -15,7 +15,8 @@ fi
 
 echo "Setting chart $CHART to version $VERSION"
 cd charts || exit $?
-find . -name 'Chart.yaml' | xargs sed -i -e "s/version: .*/version: $VERSION/g"
+find . -name 'Chart.yaml' | xargs sed -i -e "s/version: .*/version: $VERSION/g" || exit $?
+find . -name 'values.yaml' | xargs sed -i -e "s/imageTag: .*/imageTag: $VERSION/g" || exit $?
 
 echo 'Removing existing chart builds'
 rm *.tgz
