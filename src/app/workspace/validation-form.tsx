6270907688669747709a76b05fbde96ffe58508b
Ethan Bishop <epb@corefiling.com>
2017-11-28 12:35:38 +0000

    Align the upload UI more closely with the design.
    Incidentally means I can copy a lot of the diff viewer upload UI.

--- src/app/workspace/validation-form.tsx
+++ src/app/workspace/validation-form.tsx
@@ -95,8 +95,13 @@ export default class ValidationForm extends Component<ValidationFormProps, Valid
         }
       </FormItem>
       <FormItem>
-        <label htmlFor='profile-pickr'>Validation profile</label>
-        <select id='profile-pickr' disabled={!onSubmit} onChange={e => this.onChange({profile: e.currentTarget.value})}>
+        <select
+          id='profile-pickr'
+          disabled={!onSubmit}
+          required
+          defaultValue=''
+          onChange={e => this.onChange({profile: e.currentTarget.value})}>
+          <option key='' value='' disabled hidden>Validation Profile</option>
           {profiles.map(({id, name}) => <option key={id} value={id}>{name}</option>)}
         </select>
       </FormItem>
