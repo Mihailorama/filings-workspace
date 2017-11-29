bc0ae59e20f4013d350905e2dc6dada66660a9b8
kab <kab@corefiling.com>
2017-11-29 11:53:23 +0000

    Download automatically & add CSS.

--- src/app/oim-converter/oim-converter.tsx
+++ src/app/oim-converter/oim-converter.tsx
@@ -3,12 +3,16 @@ import * as React from 'react';
 import { FilingVersion } from '@cfl/document-service';
 import { documentContentURL } from './urls';
 
+import Button from '../components/button';
+
+import './oim-converter.less';
+
 interface OimConverterProps {
   filingVersion?: FilingVersion;
   message?: string;
 }
 
-export default function OimConverter(props: OimConverterProps): JSX.Element {
+function contents(props: OimConverterProps): JSX.Element {
   const { message, filingVersion } = props;
   if (message) {
     return <div>{message}</div>;
@@ -16,10 +20,23 @@ export default function OimConverter(props: OimConverterProps): JSX.Element {
   if (filingVersion) {
     const jsonDocuments = filingVersion.documents.filter(d => d.category === 'json-rendering');
     if (jsonDocuments.length > 0) {
-      const jsonDocument = jsonDocuments[0];
-      return <a href={documentContentURL(jsonDocument.id)}>Download</a>;
+      window.location.href = documentContentURL(jsonDocuments[0].id);
+      return <Button className='app-OimConverter-button'
+                     onClick={() => window.location.href = documentContentURL(jsonDocuments[0].id)}>
+              Download again
+             </Button>;
     }
     return <div>No JSON download available.</div>;
   }
   return <div>Loading...</div>;
+}
+
+export default function OimConverter(props: OimConverterProps): JSX.Element {
+  return <div className='app-OimConverter'>
+    <div className='app-OimConverter-box'>
+      <div className='app-OimConverter-title'>OIM/JSON Conversion</div>
+      {contents(props)}
+    </div>
+  </div>;
+
 }
