5a890ffee49d4e16598047ec5da39c306f82f528
Ethan Bishop <epb@corefiling.com>
2017-11-30 11:52:05 +0000

    Add filing name to OIM converter.

--- src/app/oim-converter/oim-converter.tsx
+++ src/app/oim-converter/oim-converter.tsx
@@ -1,19 +1,22 @@
 
 import * as React from 'react';
 import { FilingVersion } from '@cfl/document-service';
-import { documentContentURL } from './urls';
 
 import Button from '../components/button';
+import ContactDetails from '../components/contact-details';
+import FilingReference from '../components/filing-reference';
 
 import './oim-converter.less';
 
 interface OimConverterProps {
   filingVersion?: FilingVersion;
   message?: string;
+  onDownload: (documentId: string) => void;
 }
 
 function contents(props: OimConverterProps): JSX.Element {
-  const { message, filingVersion } = props;
+  const { message, filingVersion, onDownload } = props;
+  const name = filingVersion && filingVersion.filing.name;
   if (message) {
     return <div className='app-OimConverter-error'>
       <div className='app-OimConverter-image app-OimConverter-errorImage'/>
@@ -23,16 +26,23 @@ function contents(props: OimConverterProps): JSX.Element {
   if (filingVersion) {
     const jsonDocuments = filingVersion.documents.filter(d => d.category === 'json-rendering');
     if (jsonDocuments.length > 0) {
-      window.location.href = documentContentURL(jsonDocuments[0].id);
+      onDownload(jsonDocuments[0].id);
       return <div className='app-OimConverter-download'>
         <div className='app-OimConverter-image app-OimConverter-downloadImage'/>
         Downloaded
-        <Button primary className='app-OimConverter-downloadButton'>Download again</Button>
+        {name && <FilingReference className='app-OimConverter-filing' name={name} />}
+        <Button primary className='app-OimConverter-downloadButton'
+          onClick={() => onDownload(jsonDocuments[0].id)}>
+          Download again
+        </Button>
+        <ContactDetails className='app-OimConverter-contact' />
       </div>;
     }
     return <div className='app-OimConverter-error'>
       <div className='app-OimConverter-image app-OimConverter-errorImage'/>
       No JSON download available.
+      {name && <FilingReference className='app-OimConverter-filing' name={name} />}
+      <ContactDetails className='app-OimConverter-contact' />
     </div>;
   }
   return <div className='app-OimConverter-loading'>loading…</div>;
