1c23a92165587882e2cd87d4628ff70855329d6b
Damian Cugley <pdc@corefiling.com>
2017-12-01 11:52:01 +0000

    Nake tabs respond to the keyboard.

--- src/app/workspace/filing-list.tsx
+++ src/app/workspace/filing-list.tsx
@@ -154,7 +154,7 @@ function FilingListPageModeSelector(props: FilingListPageModeSelectorProps): JSX
       'app-FilingListPageModeChanger-selected': currentMode === requiredMode,
       'app-FilingListPageModeChanger-selected-hasLeft': hasLeft,
       'app-FilingListPageModeChanger-selected-hasRight': hasRight,
-    })} onClick={() => changeMode(requiredMode)}>{title}</a>;
+    })} onClick={e => { e.preventDefault(); changeMode(requiredMode); }} href='#' role='tab'>{title}</a>;
 }
 
 function FilingListPageModeChanger({mode, changeMode}: {mode: FilingListMode, changeMode: (mode: FilingListMode) => void}): JSX.Element {
